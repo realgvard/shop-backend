e677f34a204904a0954861cdf3e83e2f4e4e7166
Alexander Borodavka <realgvard@gmail.com>
2023-02-28 14:32:52 +0400

    Split getProductById and getProductsList by folders

---
+++ product-service/functions/getProductById.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports.handler = async (event) => {
+  const products = await import('../mocks/products.json', { assert: { type: "json" } });
+  const id = event.pathParameters.id;
+  const foundItem = products.default.find(product => product.id === id);
+
+  if (foundItem) {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify(foundItem)
+    };
+  }
+
+  return {
+    statusCode: 404,
+    body: JSON.stringify({
+      error: `Product "${id}" not found`
+    })
+  };
+};
---
+++ product-service/functions/getProductsList.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports.handler = async () => {
+  const products = await import('../mocks/products.json', { assert: { type: "json" } });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(products.default)
+  };
+};

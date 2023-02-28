927f515de4290a1b1301fad8abb0ff0e293e12cf
Alexander Borodavka <realgvard@gmail.com>
2023-02-28 13:37:48 +0400

    Create function getProductsList

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,18 +1,51 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const products = [
+  {
+    description: "Short Product Description1",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    price: 24,
+    title: "ProductOne",
+  },
+  {
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+    price: 15,
+    title: "ProductTitle",
+  },
+  {
+    description: "Short Product Description2",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+    price: 23,
+    title: "Product",
+  },
+  {
+    description: "Short Product Description4",
+    id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+    price: 15,
+    title: "ProductTest",
+  },
+  {
+    description: "Short Product Descriptio1",
+    id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+    price: 23,
+    title: "Product2",
+  },
+  {
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+    price: 15,
+    title: "ProductName",
+  },
+];
+
+module.exports.getProductsList = async () => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(products)
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };

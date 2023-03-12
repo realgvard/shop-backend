50eef6de50235ef5e3f371e30bec943f959a3a49
Alexander Borodavka <realgvard@gmail.com>
2023-03-12 21:01:36 +0400

    Update function getProductsList

--- product-service/functions/products/getProductsList.js
+++ product-service/functions/products/getProductsList.js
@@ -1,12 +1,24 @@
 'use strict';
-const products = require('../../mocks/products.json');
+const { scanProducts, scanStocks } = require("../../database/dynamoDB");
+const { joinProductsWithStocks } = require("../../database/helpers");
 
-module.exports.handler = async () => {
+
+module.exports.handler = async (event) => {
+  console.log('Log: ', event);
+
+  const products = await scanProducts();
+  const stocks = await scanStocks();
+
+  if (products.isError || stocks.isError) {
+    return {
+      statusCode: 200,
+    }
+  }
+
+  const joinedItems = joinProductsWithStocks(products.data, stocks.data);
+
   return {
     statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify(products),
+    body: JSON.stringify(joinedItems),
   };
 };
